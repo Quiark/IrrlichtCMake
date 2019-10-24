09dcbacef1bc65da780f45ad4a4c4dad561b3949
Roman Plášil <me@rplasil.name>
2019-10-24 16:49:45 +0800

    Make basic staticlib build work on MacOS

--- CMake/OperatingSystems/MacOSXSettings.cmake
+++ CMake/OperatingSystems/MacOSXSettings.cmake
@@ -20,9 +20,6 @@
 #
 
 if (APPLE)
-	message(WARNING "These MacOSX like settings have not been tested so far...")
-	message(WARNING "It might work or not, please delete this warning, when you have tested it.")
-	
 	SET(ALL_SOURCE_FILES
 		${ALL_SOURCE_FILES}
 		source/Irrlicht/MacOSX/CIrrDeviceMacOSX.mm
@@ -39,31 +36,32 @@ if (APPLE)
 	
 	INCLUDE_DIRECTORIES(
 		source/Irrlicht/MacOSX
+		source/Irrlicht
 	)
 					
 	if (GCC_LIKE_COMPILER)
 		SET (CMAKE_STATIC_LINKER_FLAGS
-			"${CMAKE_STATIC_LINKER_FLAGS} --no-export-all-symbols --add-stdcall-alias"
+			"${CMAKE_STATIC_LINKER_FLAGS}"
 		)
 		SET (CMAKE_SHARED_LINKER_FLAGS
-			"${CMAKE_SHARED_LINKER_FLAGS} --no-export-all-symbols --add-stdcall-alias"
+			"${CMAKE_SHARED_LINKER_FLAGS}"
 		)
 		SET (CMAKE_MODULE_LINKER_FLAGS
-			"${CMAKE_MODULE_LINKER_FLAGS} --no-export-all-symbols --add-stdcall-alias"
+			"${CMAKE_MODULE_LINKER_FLAGS}"
 		)
 		SET (CMAKE_EXE_LINKER_FLAGS
-			"${CMAKE_EXE_LINKER_FLAGS} --no-export-all-symbols --add-stdcall-alias"
+			"${CMAKE_EXE_LINKER_FLAGS}"
 		)
 		SET (CMAKE_CXX_FLAGS
-			"${CMAKE_CXX_FLAGS} --no-export-all-symbols --add-stdcall-alias"
+			"${CMAKE_CXX_FLAGS} -ObjC++"
 		)
 		SET (CMAKE_C_FLAGS
-			"${CMAKE_C_FLAGS} --no-export-all-symbols --add-stdcall-alias"
+			"${CMAKE_C_FLAGS} -ObjC"
 		)	
 	else ()
 		message(SEND_ERROR "I did not have non gcc like compilers in mind, when writing the MacOSX dependent settings... please adapt to your needs!")
 	endif ()	
-	
+
 	if (IRRLICHT_ENABLE_X11_SUPPORT)
 	
 		if ("$ENV{HOSTTYPE}" STREQUAL "x86_64")
@@ -74,9 +72,9 @@ if (APPLE)
 			
 	else ()	
 		if (GCC_LIKE_COMPILER)		
-			SET (CMAKE_STATIC_LINKER_FLAGS
-				"${CMAKE_STATIC_LINKER_FLAGS} -framework cocoa -framework carbon -framework opengl -framework IOKit"
-			)
+			#SET (CMAKE_STATIC_LINKER_FLAGS
+				#"${CMAKE_STATIC_LINKER_FLAGS} -framework cocoa -framework carbon -framework opengl -framework IOKit"
+			#)
 			SET (CMAKE_SHARED_LINKER_FLAGS
 				"${CMAKE_SHARED_LINKER_FLAGS} -framework cocoa -framework carbon -framework opengl -framework IOKit"
 			)
